6ecad766b98b353157d5fa95fadfc89c8253300d
Edgar Alvarez <edgarfabero85@gmail.com>
2018-04-26 18:07:07 +0200

    cambiado el constructor de la clase acceso para que coja un String y arreglado los metodos getter y setter para que no den error

--- Acceso.java
+++ Acceso.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Acceso
 {
     private int ano;
@@ -5,38 +6,64 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
+    private String cadena;
+
     
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+    public Acceso(String cadena)
     {
         this.ano = ano;
         this.mes = mes;
         this.dia = dia;
         this.hora = hora;
+        this.cadena=cadena;
         this.minutos = minutos;
+
     }
     
     public int getAno() 
     {
+
+        String [] separado = cadena.split(" ");
+ 
+        String caracter = separado[0];
+        ano = Integer.parseInt(caracter);
         return ano;
     }
     
     public int getMes()
     {
+        String [] separado = cadena.split(" ");
+ 
+        String caracter = separado[1];
+        mes = Integer.parseInt(caracter);
+        
         return mes;
     }
     
     public int getDia()
     {
+        String [] separado = cadena.split(" ");
+ 
+        String caracter = separado[2];
+        dia = Integer.parseInt(caracter);
         return dia;
     }
     
     public int getHora()
     {
+        String [] separado = cadena.split(" ");
+ 
+        String dos = separado[3];
+        hora = Integer.parseInt(dos);
         return hora;
     }
     
     public int getMinutos()
     {
+        String [] separado = cadena.split(" ");
+ 
+        String caracter = separado[4];
+        minutos = Integer.parseInt(caracter);
         return minutos;
     }
 }
